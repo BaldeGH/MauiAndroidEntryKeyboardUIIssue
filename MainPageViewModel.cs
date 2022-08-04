93475181dd0cb0f60d23bc0da705467befc9a725
BaldeGH <8539709+BaldeGH@users.noreply.github.com>
2022-08-04 12:21:58 -0700

    Add files via upload

---
+++ MainPageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Windows.Input;
+
+namespace AdroidUITest;
+internal partial class MainPageViewModel : ObservableObject
+{
+    [ObservableProperty]
+    string labelText = string.Empty;
+
+    [ObservableProperty]
+    string entryText = string.Empty;
+
+    [ObservableProperty]
+    bool hasNotRecording = false;
+    
+    [ObservableProperty]
+    bool hasRecording = true;
+    
+    [ObservableProperty]
+    bool isImportant = false;
+
+
+    [ICommand]
+    void ExecuteCmd()
+    {
+        LabelText = LabelText + "/" + entryText;
+        EntryText = string.Empty;
+    }
+
+}
